2dd65fda0c3b2a5b79d47d90fd0302d543c92fe9
Trynity Mirell <trynity@mirell.org>
2018-01-06 15:48:37 -0800

    Adjust logic to only add keys we haven't added.
    This gets annoying otherwise when we have keys with passphrases, we
    did not do that before.

--- .zsh/lib/keychain.zsh
+++ .zsh/lib/keychain.zsh
@@ -1,7 +1,14 @@
 if [[ -z $SSH_CLIENT ]]; then
   if (($+commands[keychain])); then
-  	eval $(keychain --eval)
-  fi
+		eval $(keychain --eval)
+		KEYS=('id_rsa')
+		for KEY in $KEYS; do
+			KEY_PATH="${HOME}/.ssh/${KEY}"
+			if ! ssh-add -l | grep -q "${KEYPATH}"; then
+	  			eval $(ssh-add "${KEYPATH}" &> /dev/null)
+	  		fi
+		done
+	fi
 fi
 
 #SOCK="/tmp/ssh-agent-$USER-screen"
